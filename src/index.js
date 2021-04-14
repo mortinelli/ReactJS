1cf75cdffe6e177efcfa85f0fb5f91b4e8766159
Serhii Matvieiev <matveev.servers@gmail.com>
2021-04-14 18:04:10 +0300

    React Basic 5. JSX

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    const  items = ['Go to a walk','Ride on a bike'];
+    return(
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+}
+
+const AppHeader = () => {
+    return(
+        <h1>My Todo list</h1>
+    );
+}
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search'
+    const searchStile = {
+        fontSize: '25px'
+    }
+    return (
+        <input
+            style={searchStile}
+            placeholder={searchText} />
+    );
+
+}
+
+const App = () => {
+    const value = '<scrip>alert("")<script/>';
+    return(
+        <div>
+            {value}
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList />
+        </div>
+    );
+}
+
+
+    ReactDOM.render(<App/>,
+        document.getElementById('root'));
+
